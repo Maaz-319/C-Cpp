ba77a6d4ead408136ef1c2c20d42b10452bc5266
Maaz <83403349+Maaz-319@users.noreply.github.com>
2023-09-28 16:08:38 +0500

    Update TEMPERAT.C

--- C/TurboC++(16-bit)/Temperature_Convertor/TEMPERAT.C
+++ C/TurboC++(16-bit)/Temperature_Convertor/TEMPERAT.C
@@ -4,11 +4,16 @@
 
 
 void handle_flow();
-int run_again(){
+
+
+int run_again()
+{
 	char user_ch;
 	printf("\n\n\tChoose:\n\ta) Run Again\n\tb) Exit\n\t> ");
+	
 	if((scanf(" %c", &user_ch) != 1))
         run_again();
+	
 	switch(user_ch){
 	case 'a':
 		handle_flow();
@@ -17,68 +22,86 @@ int run_again(){
 	default:
 		printf("\n\tInvalid Input!!\n\n");
 		run_again();}
+    
     return(0);
 }
 
 
-void C_to_F(float x){
+void C_to_F(float x)
+{
 	printf("\n\t%.2f Celsius --> %.2f Fahrenheit", x, (9.0/5.0*x+32));
 	printf("\n\n\tPress Enter...");
 	getch();
+	
 	run_again();
 }
 
 
-void F_to_C(float x){
+void F_to_C(float x)
+{
 	printf("\n\t%.2f Fahrenheit --> %.2f Celsius", x, (5.0/9.0*(x-32)));
 	printf("\n\n\tPress Enter...");
 	getch();
+	
 	run_again();
 }
 
 
-void C_to_K(float x){
+void C_to_K(float x)
+{
 	printf("\n\t%.2f Celsius --> %.2f Kelvin", x, x+273.15);
 	printf("\n\n\tPress Enter...");
 	getch();
+	
 	run_again();
 }
 
 
-void K_to_C(float x){
+void K_to_C(float x)
+{
 	printf("\n\t%.2f Kelvin --> %.2f Celsius", x, x-273.15);
 	printf("\n\n\tPress Enter...");
 	getch();
+	
 	run_again();
 }
 
 
-void F_to_K(float x){
+void F_to_K(float x)
+{
 	printf("\n\t%.2f Fahrenheit --> %.2f Kelvin", x, (5.0/9.0*(x-32.0))+273.15);
 	printf("\n\n\tPress Enter...");
 	getch();
+	
 	run_again();
 }
 
 
-void K_to_F(float x){
+void K_to_F(float x)
+{
 	printf("\n\t%.2f Kelvin --> %.2f Fahrenheit", x, 9.0/5.0*(x-273.15)+32.0);
 	printf("\n\n\tPress Enter...");
 	getch();
+	
 	run_again();
 }
 
 
-void handle_flow(){
+void handle_flow()
+{
 	char user_choice;
 	float temperature;
+	
 	system("cls");
 	printf("\n\tChoose:\n\t\ta) Celsius to Fahreheit\n\t\tb) Fahrenheit to Celsius\n\t\tc) Celsius to Kelvin\n\t\td) Kelvin to Celsius\n\t\te) Fahreheit to Kelvin\n\t\tf) Kelvin to Fahreheit\n\t> ");
+	
 	if((scanf(" %c", &user_choice)) != 1)
         handle_flow();
+	
 	printf("\n\tEnter Temperature: ");
 	if((scanf(" %f", &temperature)) != 1)
 		handle_flow();
+	
 	switch (user_choice)
 	{
 		case 'a':
@@ -96,13 +119,16 @@ void handle_flow(){
 			K_to_F(temperature);
 			K_to_F(temperature);
 		default:
-			handle_flow();}
+			handle_flow();
+	}
 }
 
 
-int main(){
+int main()
+{
 	printf("\n\tWelcome to Temperature Convertor | By Maaz\n\n\tPress Enter to start...");
 	getch();
 	handle_flow();
+	
 	return(0);
 }
